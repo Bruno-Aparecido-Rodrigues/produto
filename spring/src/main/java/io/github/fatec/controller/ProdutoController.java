cc9444a0a1ec57b21f616252133bfeda373f4050
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2026-04-26 13:47:36 -0300

    correção

--- spring/src/main/java/io/github/fatec/controller/ProdutoController.java
+++ spring/src/main/java/io/github/fatec/controller/ProdutoController.java
@@ -1,11 +1,12 @@
 package io.github.fatec.controller;
 
+
 import io.github.fatec.controller.adapter.ProdutoControllerAdapter;
 import io.github.fatec.controller.dto.request.ProdutoRequest;
 import io.github.fatec.controller.dto.request.ProdutoUpdateRequest;
 import io.github.fatec.controller.dto.response.ProdutoResponse;
 import io.github.fatec.entity.Produto;
-import io.github.fatec.service.ProdutoService;
+import io.github.fatec.repository.ProdutoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +16,17 @@ import java.util.List;
 @RequestMapping("/fatec/produtos")
 public class ProdutoController {
 
-    private final ProdutoService service;
+    private final ProdutoRepository repository;
 
-    public ProdutoController(ProdutoService service) {
-        this.service = service;
+    public ProdutoController(ProdutoRepository repository) {
+        this.repository = repository;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ProdutoResponse criar(@RequestBody ProdutoRequest request) {
         Produto produto = ProdutoControllerAdapter.cast(request);
-        Produto criado = service.criar(produto);
+        Produto criado = repository.salvar(produto);
         return ProdutoControllerAdapter.toResponse(criado);
     }
 
@@ -33,20 +34,20 @@ public class ProdutoController {
     @PutMapping
     public ProdutoResponse atualizar(@RequestBody ProdutoUpdateRequest request) {
         Produto produto = ProdutoControllerAdapter.cast(request);
-        Produto atualizado = service.atualizar(produto);
+        Produto atualizado = repository.atualizar(produto);
         return ProdutoControllerAdapter.toResponse(atualizado);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable String id) {
-        service.deletar(id);
+        repository.deletar(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<ProdutoResponse> listar() {
-        return service.listar()
+        return repository.listar()
                 .stream()
                 .map(ProdutoControllerAdapter::toResponse)
                 .toList();
@@ -55,7 +56,8 @@ public class ProdutoController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public ProdutoResponse buscarPorId(@PathVariable String id) {
-        Produto produto = service.buscarPorId(id);
+        Produto produto = repository.buscarPorId(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado: " + id));
         return ProdutoControllerAdapter.toResponse(produto);
     }
 }
